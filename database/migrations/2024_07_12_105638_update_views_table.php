2ac8dcafd151974c37b095d8f632d2db706e56ae
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-12 12:59:12 +0200

    feat: update views_table in migrations

--- database/migrations/2024_07_12_105638_update_views_table.php
+++ database/migrations/2024_07_12_105638_update_views_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('views', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('flat_id')->nullable();
-            $table->date('date');
-            $table->string('ip_address');
-            $table->timestamps();
-        
+        Schema::table('views', function (Blueprint $table) {
+            $table->foreign('flat_id')->references('id')->on('flats')->nullOnDelete();
         });
     }
 
@@ -26,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('views');
+        Schema::table('views', function (Blueprint $table) {
+            $table->dropColumn('flat_id');
+        });
     }
 };
