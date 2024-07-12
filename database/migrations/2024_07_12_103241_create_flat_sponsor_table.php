656a1917fd780686ad66c17625f3e7b1bb8bf911
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-12 12:39:03 +0200

    feat: flat_sponsor_table in migrations

---
+++ database/migrations/2024_07_12_103241_create_flat_sponsor_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flat_sponsor', function (Blueprint $table) {
+            $table->unsignedBigInteger('flat_id');
+            $table->foreign('flat_id')->references('id')->on('flats')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('sponsor_id');
+            $table->foreign('sponsor_id')->references('id')->on('sponsors')->cascadeOnDelete();
+
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+
+            // primary key
+            $table->primary(['flat_id', 'sponsor_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flat_sponsor');
+    }
+};
