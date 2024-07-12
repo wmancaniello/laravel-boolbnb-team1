4f8899610c6a3f6ca588af5206e87ab5de6aee8f
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-12 12:29:19 +0200

    feat: sponsors_table

---
+++ database/migrations/2024_07_12_102514_create_sponsors_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sponsors', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('price', 3, 2);
+            $table->time('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sponsors');
+    }
+};
