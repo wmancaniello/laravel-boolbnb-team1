9bda197c84d47b5c4dab518cf34479576f6ee253
wmancaniello <wmancaniello@gmail.com>
2024-07-12 13:08:01 +0200

    fix

--- database/migrations/2024_07_12_100738_create_photos_table.php
+++ database/migrations/2024_07_12_100738_create_photos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('photos', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('flat_id')->nullable()->after('id');
+            $table->unsignedBigInteger('flat_id')->nullable();
             $table->string('image');
             $table->timestamps();
             
