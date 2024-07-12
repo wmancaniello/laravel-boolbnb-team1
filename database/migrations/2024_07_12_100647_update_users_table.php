be438e1bee4db836aef4df224d13eb0070c4c42e
wmancaniello <wmancaniello@gmail.com>
2024-07-12 17:30:31 +0200

    fix nullable user

--- database/migrations/2024_07_12_100647_update_users_table.php
+++ database/migrations/2024_07_12_100647_update_users_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('surname')->after('name');
-            $table->date('date_of_birth')->after('surname');
+            $table->string('surname')->nullable()->after('name');
+            $table->date('date_of_birth')->nullable()->after('surname');
         });
     }
 
