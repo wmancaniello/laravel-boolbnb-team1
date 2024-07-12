5e099d408063ee5d7743f29edc3ce2a4257bbe4d
wmancaniello <wmancaniello@gmail.com>
2024-07-12 15:45:35 +0200

    create Model

---
+++ app/Models/Flat.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Flat extends Model
+{
+    use HasFactory;
+    
+    public function views()
+    {
+        return $this->hasMany(View::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function services()
+    {
+        return $this->belongsToMany(Service::class);
+    }
+
+    public function sponsors()
+    {
+        return $this->belongsToMany(Sponsor::class);
+    }
+}
