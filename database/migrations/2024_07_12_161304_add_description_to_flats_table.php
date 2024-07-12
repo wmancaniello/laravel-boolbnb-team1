ffb529be4db367832b9ea08c4248198be11d86af
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-07-12 18:14:48 +0200

    feat: add description on flats table

---
+++ database/migrations/2024_07_12_161304_add_description_to_flats_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('flats', function (Blueprint $table) {
+            $table->text('description')->nullable()->after('main_img');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('flats', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+};
