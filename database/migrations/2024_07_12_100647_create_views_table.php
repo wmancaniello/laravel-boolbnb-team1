883973cfcb087729f5b28e1a697d766c73af4e7c
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-12 12:24:06 +0200

    feat: views_table

---
+++ database/migrations/2024_07_12_100647_create_views_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('flet_id')->nullable()->after('id');
+            $table->date('date');
+            $table->string('ip_address');
+            $table->timestamps();
+            // foreign key
+            $table->foreign('flat_id')->references('id')->on('flats')->nullOnDelete();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('views');
+    }
+};
