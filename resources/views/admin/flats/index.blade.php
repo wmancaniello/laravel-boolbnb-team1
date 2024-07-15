c6633b8c4179aa80a635188b03e688390c88752e
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-15 15:17:38 +0200

    fix: table in index.blade

--- resources/views/admin/flats/index.blade.php
+++ resources/views/admin/flats/index.blade.php
@@ -14,37 +14,36 @@
                         <th scope="col">Titolo</th>
                         <th scope="col">Slug</th>
                         <th scope="col">Indirizzo</th>
+                        <th scope="col">Azioni</th>
                     </tr>
                 </thead>
 
                 @foreach ($flats as $flat)
                     <tbody>
-                        <tr>
+                        <tr class="h-100">
                             <th scope="row">{{ $flat->id }}</th>
                             <td>{{ $flat->title }}</td>
                             <td>{{ $flat->slug }}</td>
                             <td>{{ $flat->address }}</td>
 
-                            <td class="text-nowrap d-flex">
-
-                                <a class="btn btn-primary"
-                                    href="{{ route('admin.flats.show', ['flat' => $flat->slug]) }}">Dettagli</a>
-                                <a href="{{ route('admin.flats.edit', ['flat' => $flat->slug]) }}"
-                                    type="button"class="btn btn-outlime-primary p-0 ms-5">
-                                    <i class="fa-solid fa-pencil rounded-1 text-primary border border-primary p-2"></i>
-                                </a>
-                                <form action="{{ route('admin.flats.destroy', ['flat' => $flat->slug]) }}"
-                                    method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn p-0" data-bs-toggle="modal"
-                                        data-bs-target="#exampleModal">
-                                        <i class="fa-solid fa-trash border border-danger text-danger p-2 rounded-1"></i>
-                                    </button>
-
-                                </form>
-
-
+                            <td class="text-nowrap h-100">
+                                <div class="d-flex">
+                                    <a class="btn btn-primary"
+                                        href="{{ route('admin.flats.show', ['flat' => $flat->slug]) }}">Dettagli</a>
+                                    <a href="{{ route('admin.flats.edit', ['flat' => $flat->slug]) }}"
+                                        type="button"class="btn btn-outlime-primary p-0 ms-5">
+                                        <i class="fa-solid fa-pencil rounded-1 text-primary border border-primary p-2"></i>
+                                    </a>
+                                    <form action="{{ route('admin.flats.destroy', ['flat' => $flat->slug]) }}"
+                                        method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn p-0" data-bs-toggle="modal"
+                                            data-bs-target="#exampleModal">
+                                            <i class="fa-solid fa-trash border border-danger text-danger p-2 rounded-1"></i>
+                                        </button>
+                                    </form>
+                                </div>
                             </td>
                         </tr>
                     </tbody>
