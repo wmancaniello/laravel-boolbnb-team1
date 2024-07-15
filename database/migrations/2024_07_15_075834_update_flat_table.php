784b3441a77b393d75fe1515008391584d4d785d
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-15 10:05:44 +0200

    fix: slug in update_flat_table

--- database/migrations/2024_07_15_075834_update_flat_table.php
+++ database/migrations/2024_07_15_075834_update_flat_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('flat', function (Blueprint $table) {
-            $table->string('slug')->unique();
+            $table->string('slug')->unique()->after('title');
         });
     }
 
