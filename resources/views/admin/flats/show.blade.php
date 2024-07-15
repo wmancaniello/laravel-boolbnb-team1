f806127c1b1dd96739e67e1f07e486a5962c3f09
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-15 12:09:23 +0200

    feat: table inshow.blade in admin/flats

--- resources/views/admin/flats/show.blade.php
+++ resources/views/admin/flats/show.blade.php
@@ -3,16 +3,17 @@
 @section('content')
 
     <div class="container">
-        <h1>I TUOI APPARTAMENTI</h1>
+        <h1>I TUOI APPARTAMENTI:</h1>
 
         <div>
             <h2>{{ $flat->title }}</h2>
-            <img src="{{ asset('storage/' . $flat->main_img) }}" alt="main_img">
+            <img class="w-25" src="{{ asset('storage/' . $flat->main_img) }}" alt="main_img">
         </div>
         {{-- tabella info --}}
         <div>
             <table class="table">
                 <tbody>
+
                     <tr>
                         <th scope="row">Numero massimo di ospiti</th>
                         <td>{{ $flat->max_guests }}</td>
@@ -22,12 +23,39 @@
                         <td>{{ $flat->rooms }}</td>
                     </tr>
                     <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
+                        <th scope="row">Posti letto</th>
+                        <td>{{ $flat->beds }}</td>
                     </tr>
+
+                    <tr>
+                        <th scope="row">Bagni</th>
+                        <td>{{ $flat->bathrooms }}</td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row">Metri qudri</th>
+                        <td>{{ $flat->meters_square }}</td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row">Indirizzo</th>
+                        <td>{{ $flat->address }}</td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row">Descrizione</th>
+                        <td>{{ $flat->description }}</td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row">Visibile</th>
+                        <td>{{ $flat->visible }}</td>
+                    </tr>
+
                 </tbody>
             </table>
         </div>
 
     </div>
+
+    @endsection
