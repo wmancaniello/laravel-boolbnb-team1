633e0b83c6cbe41f8c33f788d2893698db00ce74
Alecaring <Private_a.a.c@icloud.com>
2024-07-16 16:07:11 +0200

    fix: app.js login class

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" id="login-form">
+                    <form method="POST" action="{{ route('login') }}" class="login-form">
                         @csrf
 
                         <div class="mb-4 row">
