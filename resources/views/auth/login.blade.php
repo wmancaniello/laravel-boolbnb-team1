106373ae73a60650698ad88c6b5959b2d8c495e7
Alecaring <Private_a.a.c@icloud.com>
2024-07-16 15:55:09 +0200

    Revert "fix: app.js login form class"
    This reverts commit 70bea3a23a1cb142138d8e660a59cc606ca4a969.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form class="login-form" method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('login') }}" id="login-form">
                         @csrf
 
                         <div class="mb-4 row">
