80da1b174e5168b2b2b3b7af5141b7c4ebfaa976
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-16 12:56:28 +0200

    add: back button in show.blade

--- resources/views/admin/flats/show.blade.php
+++ resources/views/admin/flats/show.blade.php
@@ -3,6 +3,13 @@
 @section('content')
 
     <div class="container">
+
+        {{-- indietro --}}
+        <a class="btn btn-primary mt-3 mb-3" href="{{ route('admin.flats.index', ['flat' => $flat->slug]) }}"
+            type="button"class="btn btn-outlime-primary p-0 ms-5">
+            <i class="fa-solid fa-arrow-left"></i>
+        </a>
+
         <h1>I TUOI APPARTAMENTI:</h1>
 
         <div>
