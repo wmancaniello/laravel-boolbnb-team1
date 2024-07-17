fb83e3166ff6a8f23e764680ebcf6ab1f8bd8ad8
wmancaniello <wmancaniello@gmail.com>
2024-07-17 15:58:04 +0200

    sabino approved

--- app/Http/Controllers/Api/FlatController.php
+++ app/Http/Controllers/Api/FlatController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Flat;
+use App\Models\Service;
 
 class FlatController extends Controller
 {
     public function index()
     {
-        $flats = Flat::all();
+        $flats = Flat::with('services')->get();
         return response()->json($flats);
     }
 }
