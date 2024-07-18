3261a3befd67c3c1ba176a9d506d3ea22800cc19
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-18 18:22:59 +0200

    fix: dashboard.blade

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,8 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8 mt-4">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
+    
                     <div class="card-body">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
@@ -14,7 +13,7 @@
                             </div>
                         @endif
 
-                        {{ __('You are logged in!') }}
+                        {{ __("Benvenuto")}} {{ Auth::user()->name }}
                     </div>
                 </div>
             </div>
