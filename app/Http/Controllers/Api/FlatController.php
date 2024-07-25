d82db4024e60b344d6558616e694edc3c98cbad7
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-07-25 12:43:48 +0200

    feat: add flag sonsored at flats with sponsor active

--- app/Http/Controllers/Api/FlatController.php
+++ app/Http/Controllers/Api/FlatController.php
@@ -6,16 +6,18 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Flat;
 use App\Models\Service;
+use Carbon\Carbon;
 
 class FlatController extends Controller
 {
     public function index(Request $request)
-    {      
-            $serviceIds = $request->services;
-            $minBeds = $request->beds;
-            $minRooms = $request->rooms;
-            
-            $flats = Flat::query()
+    {
+        $serviceIds = $request->services;
+        $minBeds = $request->beds;
+        $minRooms = $request->rooms;
+
+        $flats = Flat::query()
+            ->with('sponsors')
             ->visibleFlats()
             ->minBeds($minBeds)
             ->minRooms($minRooms)
@@ -23,14 +25,28 @@ class FlatController extends Controller
                 return $query->withAllServices($serviceIds);
             })
             ->orderBy('beds', 'asc')
+            ->orderBy('rooms', 'asc')
             ->get();
 
-        return response()->json($flats);
+        $currentDate = Carbon::now('Europe/Rome');
+        
+
+        $updatedFlats = $flats->map(function ($flat) use ($currentDate) {
+            $flatEndDate = isset($flat->sponsors[0]->pivot->end_date) ? Carbon::parse($flat->sponsors[0]->pivot->end_date, 'Europe/Rome') : Carbon::parse('1900-01-01', 'Europe/Rome');
+            
+            $flat->sponsored = $currentDate->lt($flatEndDate);
+            return $flat;
+        });
+
+        
+
+        return response()->json($updatedFlats);
     }
 
-    public function show($slug) {
+    public function show($slug)
+    {
 
-        $flat = Flat::with(['services', 'photos'])->where("slug", $slug )->first();
+        $flat = Flat::with(['services', 'photos'])->where("slug", $slug)->first();
         return response()->json($flat);
     }
 }
