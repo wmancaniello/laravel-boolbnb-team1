1038cea1215ee095f0a4f291b994172bc7ca2bee
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-07-29 17:45:46 +0200

    feat: fix icon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image" href="public/favicon.ico">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
